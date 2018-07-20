b0486deb3427483f0c4d47701bdacd39cbb5e186
prog <vlad.cv.mail@gmail.com>
2018-07-20 15:27:42 +0500

    Adding Auth with JWT example

---
+++ JWT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace JWT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var webHost = WebHost.CreateDefaultBuilder(args) 
+                .ConfigureAppConfiguration((webHostBuilderContext, configurationbuilder) =>  
+                {  
+                    var environment = webHostBuilderContext.HostingEnvironment; 
+                    string envName = environment.EnvironmentName;
+                     
+                    string appsettingsPath = Path.Combine(environment.ContentRootPath,"appsettings");  
+                    configurationbuilder  
+                            .AddJsonFile(Path.Combine(appsettingsPath,"appsettings.json"), optional: false, reloadOnChange: true)  
+                            .AddJsonFile(Path.Combine(appsettingsPath,$"appsettings.{envName}.json"), optional: true, reloadOnChange: true);
+                })  
+                .UseStartup<Startup>();
+            return webHost;            
+        }             
+    }
+}
---
+++ JWT/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT
+{
+    public class Startup    
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration conf)
+        {
+            Configuration = conf;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.
+                AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:ServerSecret"]));
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = serverSecret,
+                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidAudience = Configuration["JWT:Audience"]
+                    };
+                });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
